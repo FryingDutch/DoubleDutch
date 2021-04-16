e98fab3196a8124dde6ba7d04b72436a4447f006
LyingDutch <73080273+LyingDutch@users.noreply.github.com>
2021-04-16 13:59:46 +0200

    Update LDserver.h

--- LDserver.h
+++ LDserver.h
@@ -5,53 +5,32 @@
 #include <boost/thread.hpp>
 #include <boost/bind.hpp>
 
-class LDserver
+namespace LyingD
 {
-public:
-	boost::mutex global_stream_lock;
-	void workerThread(boost::shared_ptr<boost::asio::io_service> io_service)
+	class LDserver
 	{
-		//LOG("Start of Thread");
-		for (;;)
-		{
-			try
-			{
-				boost::system::error_code ec;
-				io_service->run(ec);
-				if (ec)
-				{
-					this->global_stream_lock.lock();
-					std::cout << "[" << boost::this_thread::get_id() << "] " << ec << std::endl;
-					this->global_stream_lock.unlock();
-				}
+	private:
+		boost::thread_group worker_threads;
+		std::shared_ptr<boost::asio::io_service> io_service = std::make_shared<boost::asio::io_service>();
+		std::shared_ptr<boost::asio::io_service::work> work = std::make_shared<boost::asio::io_service::work>(*this->io_service);
+		boost::asio::io_service::strand strand = boost::asio::io_service::strand(*this->io_service);
 
-				break;
-			}
+	private:
+		void LOG(const char* message);
+		void printNum(int x);
+		void workerThread();
+		void createThreads(unsigned int amountOfThreads);
+		void raiseException();
 
-			catch (std::exception& ex)
-			{
-				this->global_stream_lock.lock();
-				std::cout << "[" << boost::this_thread::get_id() << "] " << ex.what() << std::endl;
-				this->global_stream_lock.unlock();
-			}
-		}
-		//LOG("Thread Finished");
-	}
+	public:
+		boost::mutex global_stream_lock;
 
-public:
-	LDserver() = default;
+	public:
+		LDserver(int amountOfThreads);
+		~LDserver();
 
-	void createThreads()
-	{
-		this->worker_threads.create_thread(boost::bind(&LDserver::workerThread, this->io_service));
-	}
-
-	~LDserver() = default;
-	boost::thread_group worker_threads;
-	boost::shared_ptr<boost::asio::io_service> io_service = boost::make_shared<boost::asio::io_service>();
-	boost::shared_ptr<boost::asio::io_service::work> work = boost::make_shared<boost::asio::io_service::work>(*this->io_service);
-	boost::asio::io_service::strand strand = boost::asio::io_service::strand(*this->io_service);
-};
-
+		void postPrintNum(int x);
+	};
+}
 
 
