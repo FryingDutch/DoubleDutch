e1e6ee317ee3f4ffac9580e1b5a4032e7f13fe4b
LyingDutch <73080273+LyingDutch@users.noreply.github.com>
2021-04-20 00:45:54 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,20 +5,12 @@
 #include <boost/bind.hpp>
 #include "LDserver.h"
 
+LyingD::LDserver s1(2);
+
 int main(int argc, char* argv[])
-{
-	LyingD::LDserver s1(2);
-
-	s1.global_stream_lock.lock();
-	std::cout << "[" << boost::this_thread::get_id() << "] This wil exit when all work is finished" << std::endl;	
-	s1.global_stream_lock.unlock();
-
-	s1.postPrintNum(1);
-	s1.postPrintNum(2);
-	s1.postPrintNum(3);
-	s1.postPrintNum(4);
-
-//	s1.io_service->post(boost::bind(&LDserver::raiseException, &s1));
+{	
+	s1.mainThread();
+	//s1.io_service->post(boost::bind(&LDserver::raiseException, &s1));
 
 	return 0;
 }
