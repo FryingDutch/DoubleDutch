609d48acfb952d5c87df7bf19a1e9c83aae67fb7
LyingDutch <73080273+LyingDutch@users.noreply.github.com>
2021-06-08 00:04:06 +0200

    Update LMserver.cpp

--- src/LMserver.cpp
+++ src/LMserver.cpp
@@ -1,78 +1,75 @@
 #include <vector>
+#include <string>
+#include <random>
 #include "LMserver.h"
 #include "crow.h"
 
 LMserver::LMserver(unsigned int portNum)
 {
   crow::SimpleApp app;
-  CROW_ROUTE(app, "/")([]() { return "Hello LockWorld"; });
+  CROW_ROUTE(app, "/")([]() { return "Welcome to LockManager"; });
 
-  //need to figure out how to return a full json template in this
-  CROW_ROUTE(app, "/status/json")
+  //need to figure out how to return an actual json
+  CROW_ROUTE(app, "/status")
   ([&] {
-    crow::json::wvalue x;
-    for (unsigned int i = 0; i < lockVector.size(); i++) {
-      /*
-      x[lockVector[i].getName()] = "NAME"; 
-      x[lockVector[i].getuser_id()] = "USER_ID"; 
-      */
+    std::string tempString;
+    for (unsigned int i = 0; i < this->lockVector.size(); i++) {
+      tempString = tempString + "LOCK= " + this->lockVector[i].getName() + "\n" + "USER_ID= " + this->lockVector[i].getUser_id() + "\n\n";
     }
-    return x;
+    return tempString;
   });
 
-  CROW_ROUTE(app, "/getLock/<string>/<int>")
-  ([&](std::string lockName, unsigned int user_id) {
-    if (!this->getLock(lockName, user_id)) {
-      return "false";
-    }
+  //Getting the lock
+  CROW_ROUTE(app, "/getLock/<string>")
+  ([&](std::string lockName) {
+      this->storageMutex.lock();
+      for (long unsigned int i = 0; i < this->lockVector.size(); i++) {
+        if (lockName == this->lockVector[i].getName()) {
+          this->storageMutex.unlock();
+          std::string ep = "false";
+          return ep;
+        }
+      }
 
-    else {
-      return "true";
-    }
+      std::string newID = this->createHash();
+      Lock *tempLock = new Lock(lockName, newID);
+      this->lockVector.push_back(*tempLock);
+      delete tempLock;
+
+      this->storageMutex.unlock();
+      return newID;
   });
 
-  CROW_ROUTE(app, "/releaseLock/<string>/<int>")
-  ([&](std::string lockName, unsigned int user_id) {
-    if (this->releaseLock(lockName, user_id)) {
-      return "Released";
+  //Releasing the lock
+  CROW_ROUTE(app, "/releaseLock/<string>/<string>")
+  ([&](std::string lockName, std::string user_id) {
+    this->storageMutex.lock();
+    for (long unsigned int i = 0; i < this->lockVector.size(); i++) {
+      if (lockName == this->lockVector[i].getName() &&
+          user_id == this->lockVector[i].getUser_id()) {
+        this->lockVector.erase(this->lockVector.begin() + i);
+        this->storageMutex.unlock();
+        return "released";
+      }
     }
-
-    else {
-      return "NF";
-    }
+    this->storageMutex.unlock();
+    return "false";
   });
 
   app.port(portNum).multithreaded().run();
 }
 
-bool LMserver::getLock(std::string lockName, unsigned int user_id)
+std::string LMserver::createHash() 
 {
-  this->storageMutex.lock();
-  for (long unsigned int i = 0; i < this->lockVector.size(); i++) {
-    if (lockName == lockVector[i].getName()) {
-      this->storageMutex.unlock(); 
-      return false;
+  static std::string str =
+      "01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  std::random_device rd;
+  std::mt19937 generator(rd());
+  std::shuffle(str.begin(), str.end(), generator);
+  for (unsigned int i = 0; i < this->lockVector.size(); i++) {
+    if (str.substr(0, 32) == this->lockVector[i].getUser_id()) {
+      this->createHash();    
     }
   }
-
-  Lock *tempLock = new Lock(lockName, user_id);
-  this->lockVector.push_back(*tempLock);
-  delete tempLock;
-
-  this->storageMutex.unlock();
-  return true;
-}
-
-bool LMserver::releaseLock(std::string lockName, unsigned int user_id)
-{
-  this->storageMutex.lock();
-  for (long unsigned int i = 0; i < this->lockVector.size(); i++) {
-    if (lockName == lockVector[i].getName() && user_id == lockVector[i].getUser_id()) {
-      this->lockVector.erase(this->lockVector.begin() + i);
-      this->storageMutex.unlock();
-      return true;
-    }
-  }
-  this->storageMutex.unlock();
-  return false;
+  return str.substr(0, 32);
 }
