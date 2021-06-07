aea4710d8309877e697a93254fd33c0e9a6d4a18
LyingDutch <73080273+LyingDutch@users.noreply.github.com>
2021-06-07 16:24:42 +0200

    Update Lock.h

--- src/Lock.h
+++ src/Lock.h
@@ -2,13 +2,15 @@
 #define LOCK_H
 #include <string>
 
-struct Lock
+class Lock
 {
- public:
+ private:
   std::string name;
   unsigned int user_id;
 
  public:
   Lock(std::string _name, unsigned int _id) : name(_name), user_id(_id){};
+  std::string getName() { return this->name; }
+  unsigned int getUser_id() { return this->user_id; }
 };
 #endif
