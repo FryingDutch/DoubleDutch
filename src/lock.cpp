0a6c5b3941dae1fa73c9de53bd9cc2d7c2ed2321
LyingDutch <73080273+LyingDutch@users.noreply.github.com>
2021-06-12 00:19:47 +0200

    Update lock.cpp
    Created .cpp and added lock lifetime

--- src/lock.cpp
+++ src/lock.cpp
@@ -1,1 +1,24 @@
 #include "Lock.h"
+
+namespace DoubleD
+{
+	Lock::Lock(std::string _name, std::string _id, double _lifeTime)
+		: m_name(_name), m_user_id(_id), m_lifeTime(_lifeTime)
+	{
+		m_start = std::chrono::high_resolution_clock::now();
+	}
+
+	bool Lock::m_expired()
+	{
+		auto currentTime = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double> difference = currentTime - m_start;
+		if (difference.count() > this->m_lifeTime)
+		{ return true; }
+
+		else
+		{ return false;	}
+	}
+
+	std::string Lock::m_getName() { return this->m_name; }
+	std::string Lock::m_getUser_id() { return this->m_user_id; }
+}
