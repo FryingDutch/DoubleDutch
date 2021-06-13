48b55b605a6b20898f535806420a060a0910b0ff
LyingDutch <73080273+LyingDutch@users.noreply.github.com>
2021-06-13 14:33:00 +0200

    Update LMserver.h
    added request timeout

--- src/LMserver.h
+++ src/LMserver.h
@@ -1,6 +1,6 @@
 #pragma once
-#ifndef LMSERVER_H
-#define LMSERVER_H
+#ifndef DDserver_H
+#define DDserver_H
 #include <boost/thread.hpp>
 #include <string>
 #include <vector>
@@ -8,18 +8,17 @@
 
 namespace DoubleD
 {
-	class LMserver
+	class DDserver
 	{
 	private:
-		static bool isRunning;
 		static boost::mutex m_storageMutex;
 		static std::vector<Lock> m_lockVector;
 
 	private:
-		static std::string m_createID();
+		static bool m_reqTimedout(unsigned int timeout, std::string lockName);
 
 	public:
-		LMserver();
+		DDserver();
 		static void m_startup(const unsigned int portNum);
 		static void m_checkLifetimes();
 	};
