4438ebe96b13c47adc77f7c7cc8cc44797047893
LyingDutch <73080273+LyingDutch@users.noreply.github.com>
2021-06-13 15:02:46 +0200

    Update pythonRequest.py

--- examples/pythonRequest.py
+++ examples/pythonRequest.py
@@ -3,13 +3,9 @@ key = "ans"
 
 i = 0;
 while i < 1000:
-    r = requests.get("http://localhost:8000/getLock/Hello")
-    while r.text == "false" or r.status_code != 200:
-           r = requests.get("http://localhost:8000/getLock/Hello")
-           print(r.text)
-    else:
+    r = requests.get("http://localhost:8000/getLock/3/Hello/20")
+    if r.text != "false":
            key = r.text
-           print(key)
 
     r = requests.get("http://localhost:8000/releaseLock/Hello/"+key)
     i = i + 1
