8766400cabcab49214b79584799d2928987249d6
LyingDutch <73080273+LyingDutch@users.noreply.github.com>
2021-06-13 14:34:08 +0200

    Update Lock.h
    moved the creation of the ID inside the constructor of the lock

--- src/Lock.h
+++ src/Lock.h
@@ -10,13 +10,16 @@ namespace DoubleD
     private:
         std::string m_name;
         std::string m_user_id;
-
-    public:
-        double m_lifeTime;
         std::chrono::_V2::system_clock::time_point m_start;
 
     public:
-        Lock(std::string _name, std::string _id, double _lifeTime);
+        double m_lifeTime;
+
+    private:
+        std::string m_createID();
+
+    public:
+        Lock(std::string _name, double _lifeTime);
         bool m_expired();
         std::string m_getName();
         std::string m_getUser_id();
