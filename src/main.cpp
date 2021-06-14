6ddda2580ccce1d2b622bcd827b71f6aa8950d59
LyingDutch <73080273+LyingDutch@users.noreply.github.com>
2021-06-14 17:25:15 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,33 @@
 #define CROW_MAIN 
 #include <thread>
+#include <string>
+#include <iostream>
 #include "DDserver.h"
+
+bool isString(std::string str)
+{
+	for (long unsigned int i = 0; i < str.length(); i++)
+	{
+		if (!std::isdigit(str[i]))
+		{
+			return false;
+		}
+	}
+	return true;
+}
 
 int main() 
 { 
-	std::thread th1(&DoubleD::DDserver::m_checkLifetimes);
-	DoubleD::DDserver::m_startup(8000);	
+	std::string input;
+
+	do {
+		std::cout << "Choose a portnumber: ";
+		std::getline(std::cin, input);
+	} while (!isString(input));
+
+	int port = std::stoi(input);
+
+	std::thread th1(&DoubleD::DDserver::m_startup, port); 
+	DoubleD::DDserver::m_checkLifetimes();
 	th1.join();
 }
