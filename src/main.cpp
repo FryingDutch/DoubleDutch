19ef8f5a9ba079f8526779e50f38e31a18ebb52a
LyingDutch <73080273+LyingDutch@users.noreply.github.com>
2021-06-16 17:12:48 +0200

    Update main.cpp
    Add custom amount of threads

--- src/main.cpp
+++ src/main.cpp
@@ -25,16 +25,36 @@ int main(int argc, char* argv[])
 			int port = std::stoi(argv[1]);
 			if (port > 0)
 			{
-			std::thread th1(&DoubleD::DDserver::m_startup, port);
-			DoubleD::DDserver::m_checkLifetimes();
-			th1.join();
+				int numOfThreads = std::thread::hardware_concurrency();
+				std::thread th1(&DoubleD::DDserver::m_startup, port, numOfThreads);
+				DoubleD::DDserver::m_checkLifetimes();
+				th1.join();
 			}
-
-			else { std::cout << "ERROR: Not a valid([UNSIGNED]) digit. Terminating...\n"; }
+			else { std::cout << "[ERROR]: Not a valid([UNSIGNED]) digit. Terminating...\n"; }
 		}
-		else { std::cout << "ERROR: Not a digit. Terminating...\n"; }
+		else { std::cout << "[ERROR]: Not a digit. Terminating...\n"; }
 	}
-	else { std::cout << "ERROR: No valid argument([PORTNUM]) has been given. Terminating...\n"; }
+
+	else if (argc == 3)
+	{
+		if (isDigit(argv[1]) && isDigit(argv[2]))
+		{
+			int port = std::stoi(argv[1]);
+			int numOfThreads = std::stoi(argv[2]);
+
+			if (port > 0 && numOfThreads > 0)
+			{				
+				std::thread th1(&DoubleD::DDserver::m_startup, port, numOfThreads);
+				DoubleD::DDserver::m_checkLifetimes();
+				th1.join();
+			}
+			else { std::cout << "[ERROR]: Not a valid([UNSIGNED]) digit. Terminating...\n"; }
+		}
+		else { std::cout << "[ERROR]: Not a digit. Terminating...\n"; }
+	}
+
+	else { std::cout << "[ERROR]: No valid argument([PORTNUM]) has been given. Terminating...\n"; }
+
 
 	return 0;
 }
