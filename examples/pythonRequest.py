ebc293783af005707276024356316b30160dea5c
LyingDutch <73080273+LyingDutch@users.noreply.github.com>
2021-06-17 14:45:13 +0200

    Updated the example

--- examples/pythonRequest.py
+++ examples/pythonRequest.py
@@ -3,7 +3,9 @@ key = "ans"
 
 i = 0
 while i < 1000:
-    r = requests.get("http://localhost:8000/getLock/3/Hello/20")
+    #note that lifetime is only used when the lock is never released by the client, for whatever reason. So make sure you take a widerange of your maximum work time,
+    #so its not actually just a spike in your workload and the lock releases while you are actually still doing work.
+    r = requests.get("http://localhost:8000/getLock?lockname=hello&timeout=10&lifetime=60")
     if r.text != "false":
            key = r.text
 
