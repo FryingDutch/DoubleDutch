24d37e43d778e28cb20fff55182240fb02b837a5
LyingDutch <73080273+LyingDutch@users.noreply.github.com>
2021-06-21 19:17:31 +0200

    rename variables
    makes the code more readable

--- src/Lock.h
+++ src/Lock.h
@@ -8,18 +8,18 @@ namespace DoubleD
     class Lock
     {
     private:
-        std::string m_name, m_user_id;
+        std::string m_name, m_session_token;
         double m_lifeTime;
         std::chrono::_V2::system_clock::time_point m_start;
 
     private:
-        std::string m_createID();
+        std::string m_createToken();
 
     public:
         Lock(std::string _name, double _lifeTime);
         bool m_expired();
         std::string m_getName();
-        std::string m_getUser_id();
+        std::string m_getSessionToken();
     };
 }
 
