16d6bbfe676f8402583bba0cb407d06267b64bb2
LyingDutch <73080273+LyingDutch@users.noreply.github.com>
2021-06-27 01:25:40 +0200

    now with json

--- examples/pythonRequest.py
+++ examples/pythonRequest.py
@@ -1,13 +1,7 @@
 import requests
-key = "ans"
 
-i = 0
-while i < 1000:
-    #note that lifetime is only used when the lock is never released by the client, for whatever reason. So make sure you take a widerange of your maximum work time,
-    #so its not actually just a spike in your workload and the lock releases while you are actually still doing work.
-    r = requests.get("http://localhost:8000/getLock?lockname=hello&timeout=10&lifetime=60")
-    if r.text != "false":
-           key = r.text
+data = requests.get("http://localhost:8000/getLock?auth=randomapikey&lockname=hello&timeout=3&lifetime=10").json()
+key = data["DoubleDutch/v0.1"]       
+#print(key)
 
-    r = requests.get("http://localhost:8000/releaseLock/Hello/"+key)
-    i = i + 1
+data = requests.get("http://localhost:8000/releaseLock?lockname=hello&key="+key).json()
