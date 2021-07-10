2cfd002bfbeb9fe1c4b4b6562030bacb1505f14a
FryingDutch <73080273+FryingDutch@users.noreply.github.com>
2021-07-10 16:16:16 +0200

    syntax update

--- examples/pythonRequest.py
+++ examples/pythonRequest.py
@@ -1,7 +1,7 @@
 import requests
 
-data = requests.get("http://localhost:8000/getLock?auth=randomapikey&lockname=hello&timeout=3&lifetime=10").json()
-key = data["DoubleDutch/v0.1"]       
+data = requests.get("http://localhost:8000/getlock?auth=randomapikey&lockname=hello&timeout=3&lifetime=10").json()
+token = data["DoubleDutch/v0.1"][0]       
 #print(key)
 
-data = requests.get("http://localhost:8000/releaseLock?lockname=hello&key="+key).json()
+data = requests.get("http://localhost:8000/releaselock?lockname=hello&token="+token).json()
