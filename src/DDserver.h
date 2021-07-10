b09b1fc38fca8166730f79fef5a02bb7d7a06809
FryingDutch <73080273+FryingDutch@users.noreply.github.com>
2021-07-10 16:07:36 +0200

    Update DDserver.h

--- src/DDserver.h
+++ src/DDserver.h
@@ -1,6 +1,4 @@
 #pragma once
-#ifndef DDserver_H
-#define DDserver_H
 #include <boost/thread.hpp>
 #include <string>
 #include <vector>
@@ -28,7 +26,7 @@ namespace DoubleD
 
 		//functions that are being used to define runtime situations
 		static void m_startup();
-		static bool m_reqTimedout(unsigned int timeout, std::string lockName);
+		static std::string m_handleRequest(const unsigned int TIMEOUT, std::string lockName, const double LIFETIME);
 
 		//dedicated thread
 		static void m_checkLifetimes();
@@ -38,5 +36,3 @@ namespace DoubleD
 		static void m_setAndBoot(int _argc, char* _argv[]);
 	};
 }
-
-#endif
