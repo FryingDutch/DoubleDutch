86f782077fe38fb100563d625cc11f8db53efcb1
FryingDutch <73080273+FryingDutch@users.noreply.github.com>
2021-07-26 01:48:12 +0200

    remove m_

--- src/DDserver.h
+++ src/DDserver.h
@@ -9,31 +9,31 @@ namespace DoubleD
 	class DDserver
 	{
 	private:
-		static std::string m_server_name, m_crt_file_path, m_key_file_path, m_api_key;
-		static int m_port, m_precision, m_threads;
-		static bool m_is_https, m_error, m_isRunning, m_custom_api_key;
+		static std::string server_name, crt_file_path, key_file_path, api_key;
+		static int port, precision, threads;
+		static bool is_https, error, isRunning, custom_api_key;
 
 	private:
-		static boost::mutex m_storageMutex;
-		static std::vector<Lock> m_lockVector;
+		static boost::mutex storageMutex;
+		static std::vector<Lock> lockVector;
 
 	private:
 		//functions used to define the settings of the server
-		static void m_errormsg(const char* message);
-		static bool m_isDigit(std::string str);
-		static void m_handleCommandLineArguments(char* _argv[], int _argc);
-		static void m_loadApiKey();
+		static void errormsg(const char* message);
+		static bool isDigit(std::string str);
+		static void handleCommandLineArguments(char* _argv[], int _argc);
+		static void loadApiKey();
 
 		//functions that are being used to define runtime situations
-		static void m_startup();
-		static boost::optional<Lock> m_handleRequest(std::string lockName, const unsigned int TIMEOUT, const double LIFETIME);
-		static boost::optional<Lock> m_getLock(std::string lockName, const double LIFETIME);
+		static void startup();
+		static boost::optional<Lock> handleRequest(std::string lockName, const unsigned int TIMEOUT, const double LIFETIME);
+		static boost::optional<Lock> getLock(std::string lockName, const double LIFETIME);
 
 		//dedicated thread
-		static void m_checkLifetimes();
+		static void checkLifetimes();
 
 	public:
 		DDserver();
-		static void m_setAndBoot(int _argc, char* _argv[]);
+		static void setAndBoot(int _argc, char* _argv[]);
 	};
 }
