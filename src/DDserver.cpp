a83ca22f7391aa9f5730a23351d19029b0b591f1
FryingDutch <abbring_93@outlook.com>
2021-11-05 14:09:12 +0100

    update version displayed

--- src/DDserver.cpp
+++ src/DDserver.cpp
@@ -12,7 +12,7 @@
 
 namespace DoubleD
 {
-    std::string DDserver::server_name = "DoubleDutch/v0.2.3";
+    std::string DDserver::server_name = "DoubleDutch/v0.3.0";
     std::string DDserver::crt_file_path = "/certificate.crt";
     std::string DDserver::key_file_path = "/privateKey.key";
     std::string DDserver::api_key;
