653395dd747204e9aa3b5ac744c75e50c70c3b1e
FryingDutch <abbring_93@outlook.com>
2021-11-10 12:30:01 +0100

    error checking JSON return

--- src/Request.h
+++ src/Request.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <iostream>
+#include <string>
 #include <cpr/cpr.h>
 #include "crow.h"
 
@@ -6,13 +8,16 @@ namespace DoubleD
 {
 	struct Request
 	{
-	public:
+	private:
+		uint32_t status_code;
 		std::string text;
 		crow::json::wvalue json;
 
 	public: 
 		Request& GET(const char* _URL);
 		void POST(const char* _URL, std::string _apikey, std::string _lockname);
-		crow::json::wvalue JSON();
+		crow::json::wvalue& JSON();
+		std::string& string();
+		uint32_t& statusCode();
 	};
 }
--- src/request.cpp
+++ src/request.cpp
@@ -9,22 +9,42 @@ namespace DoubleD
 	{
 		cpr::Response response = cpr::Get(cpr::Url{ _URL });
 		text = response.text;
+		status_code = response.status_code;
 
-		auto x = crow::json::load(response.text);
-		json = x;
-		
+		if (status_code == 200)
+		{
+			try
+			{
+				json = crow::json::load(response.text);
+			}
+
+			catch (...)
+			{
+				std::cerr << "[Request]: Not a valid JSON return!\n";
+			}
+		}
 		return *this;
-	}
-
-	crow::json::wvalue Request::JSON()
-	{
-		return json;
 	}
 
 	void Request::POST(const char* _URL, std::string _apikey, std::string _lockname)
 	{
 		cpr::Response response = cpr::Post(cpr::Url{ _URL },
-			cpr::Payload{ {"auth", _apikey},{"lockname", _lockname }}
+			cpr::Payload{ {"auth", _apikey},{"lockname", _lockname } }
 		);
+	}
+
+	crow::json::wvalue& Request::JSON()
+	{
+		return json;
+	}
+	
+	std::string& Request::string()
+	{
+		return text;
+	}
+
+	uint32_t& Request::statusCode()
+	{
+		return status_code;
 	}
 }
